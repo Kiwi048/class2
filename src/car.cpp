93e02f82230ea664eb4db3489ce13d5e3dc6c91e
kiwi048 <15184473+kiwi048@user.noreply.gitee.com>
2024-11-25 23:09:50 +0800

    4

--- src/car.cpp
+++ src/car.cpp
@@ -8,7 +8,7 @@ int main()
     Executor executor;
     std::string commands;
 
-    std::cout << "Enter commands (M to move, L to turn left, R to turn right, Q to quit): " << std::endl;
+    std::cout << "输入指令, M前进, L左转, R右转, Q退出: " << std::endl;
     while (true) {
         std::cout << "> ";
         std::cin >> commands;
